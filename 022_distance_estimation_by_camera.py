ba07d269497e15344f50eedc21ffedc8e0e53524
aki-tera <aki.tera@gmail.com>
2021-08-20 15:29:17 +0900

    Ver1.6 test

--- 022_distance_estimation_by_camera.py
+++ 022_distance_estimation_by_camera.py
@@ -1,6 +1,11 @@
+import tkinter as tk
+from PIL import Image, ImageTk
+from tkinter import font
+
 import cv2
+import numpy as np
+
 import json
-from logging import DEBUG, INFO, WARNING, ERROR, CRITICAL
 
 
 # PEP8に準拠するとimportが先頭に行くので苦肉の策
@@ -8,101 +13,359 @@ while True:
     import sys
     sys.path.append("../000_mymodule/")
     import logger
+    from logging import DEBUG, INFO, WARNING, ERROR, CRITICAL
     break
 
 
-def estimation(setting):
-    log = logger.Logger("estimation", level=INFO)
+class Model:
+    log = logger.Logger("Model", level=DEBUG)
 
+    def __init__(self):
+        self.log.debug("__init__")
+        self.distance_x = 999.0
+        self.distance_y = 999.0
+        self.distance_xy = 999.0
 
-    aruco = cv2.aruco
-    dictionary = aruco.getPredefinedDictionary(aruco.DICT_4X4_50)
+    def camera_open(self, camera_setting):
+        # カメラ起動
+        self.log.debug("camera_open")
 
-    cap = cv2.VideoCapture(setting["CAM"]["ID"])
+        self.camera_setting = camera_setting
 
-    log.debug(setting["CAM"]["ID"])
+        self.aruco = cv2.aruco
+        self.dictionary = self.aruco.getPredefinedDictionary(self.aruco.DICT_4X4_50)
 
-    window_width = setting["CAM"]["WIDTH"]
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, window_width)
+        self.cap = cv2.VideoCapture(self.camera_setting["CAM"]["ID"])
+        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, self.camera_setting["CAM"]["WIDTH"])
+    
+    def caremra_release(self):
+        # カメラリソース解放
+        self.log.debug("camera_release")
+        self.cap.release()
 
-    id_start = setting["TARGET"]["START"]
-    id_end = setting["TARGET"]["END"]
+    def create_camera_infomations(self):
+        # cv2の処理をすべて実施
+        self.log.debug("create_camera_infomations")
 
-    log.debug(window_width)
-    log.debug(id_start)
-    log.debug(id_end)
-
-    while True:
         # ビデオキャプチャから画像を取得
-        # ret:True/False
-        # frame：画像データ
-        ret, frame = cap.read()
+        ret, frame = self.cap.read()
+
+        self.log.debug(ret)
 
         # sizeを取得
         # (縦、横、色)
+
         Height, Width = frame.shape[:2]
 
-        log.debug(Height)
-        log.debug(Width)
+        img0 = cv2.resize(frame, (int(200), int(200 * Height / Width)))
+        img1 = cv2.resize(frame, (int(Width), int(Height)))
 
-        img = cv2.resize(frame, (int(Width), int(Height)))
-
-        # マーカを検出
-        corners, ids, rejectedImgPoints = aruco.detectMarkers(img, dictionary)
-
-        log.debug(corners)
-        log.debug(ids)
+        # 検出
+        corners, ids, rejectedImgPoints = self.aruco.detectMarkers(img1, self.dictionary)
 
         # 検出したマーカに描画する
-        aruco.drawDetectedMarkers(img, corners, ids, (0, 255, 0))
+        # イメージをコピーする
+        self.img2 = np.copy(img1)
+        self.aruco.drawDetectedMarkers(self.img2, corners, ids, (0, 255, 0))
+        self.img2 = cv2.resize(self.img2, (int(400), int(400 * Height / Width)))
 
-        # ラインを引く
+        self.log.debug(f"x:400 y:{int(400* Height / Width)}")
+
+        m = np.empty((4, 2))
+
+        self.log.debug(ids)
+        self.log.debug(type(ids))
+
+        # ラインを引く準備
         x_start, y_start = 0, 0
         x_end, y_end = 0, 0
 
-        try:
-            for number, corner in zip(ids, corners):
+        if ids is None:
+            self.log.debug("None")
+            img_trans = cv2.resize(frame, (600, 600))
+            self.distance_x = 0.0
+            self.distance_y = 0.0
+            self.distance_xy = 0.0
+            
+        elif np.count_nonzero((2 <= ids) & (ids <= 6)) == 4:
+            self.log.debug("Number is 4")
+            for i, c in zip(ids, corners):
+                # マーカーの赤丸位置を基準としている
+                if 2 <= i <= 6:
+                    self.log.debug(c)
+                    m[i - 2] = c[0][0]
 
-                log.debug(number)
+            # 変形後の画像サイズ
+            trans_width, trans_height = (600, 600)
 
-                if number == id_start:
-                    x_start, y_start = int(corner[0][0][0]), int(corner[0][0][1])
-                elif number == id_end:
-                    x_end, y_end = int(corner[0][0][0]), int(corner[0][0][1])
-        except:
-            pass
+            marker_coordinates = np.float32(m)
+            true_coordinates = np.float32([[0, 0], [trans_width, 0], [trans_width, trans_height], [0, trans_height]])
+            trans_mat = cv2.getPerspectiveTransform(marker_coordinates, true_coordinates)
+            self.img_trans = cv2.warpPerspective(img1, trans_mat, (trans_width, trans_height))
 
-        # 0と2の赤角距離を表示
-        x, y = (x_end - x_start), (y_end - y_start)
+            if np.count_nonzero((0 <= ids) & (ids <= 1)) == 2:
+                # 変換後の画像でid検索
+                corners_trans, ids_trans, rejectedImgPoints_trans = self.aruco.detectMarkers(self.img_trans, self.dictionary)
+                # id(0と1)の抽出
+                if ids_trans is not None:
+                    for number, corner in zip(ids_trans, corners_trans):
+                        if number == 0:
+                            x_start, y_start = int(corner[0][0][0]), int(corner[0][0][1])
+                        elif number == 1:
+                            x_end, y_end = int(corner[0][0][0]), int(corner[0][0][1])
+                    self.log.debug(x_start)
+                    self.log.debug(y_start)
+                    self.log.debug(x_end)
+                    self.log.debug(y_end)
+                    # id(0と1)の距離を表示
+                    self.distance_x, self.distance_y = (x_end - x_start), (y_end - y_start)
+                    self.log.info(self.distance_x)
+                    self.log.info(self.distance_y)
+                    self.distance_xy = ((self.distance_x * self.distance_x + self.distance_y * self.distance_y) ** 0.5) * 135 / 600
+                    self.log.info(self.distance_xy)
 
-        log.info(x)
-        log.info(y)
+                    cv2.line(self.img_trans, (x_start, y_start), (x_end, y_end), (0, 0, 255), 1)
 
-        cv2.line(img, (x_start, y_start), (x_end, y_end), (0, 0, 255), 1)
-        cv2.putText(img, str(f"x:{x:.1f}, y:{y:.1f}"), (0, Height - 10), cv2.FONT_HERSHEY_PLAIN, 3, (255, 255, 255), 3, cv2.LINE_AA)
+        else:
+            self.log.debug("others")
+            self.img_trans = cv2.resize(frame, (600, 600))
+            self.distance_x = 0.0
+            self.distance_y = 0.0
+            self.distance_xy = 0.0
+            
 
-        # マーカが描画された画像を表示
-        cv2.imshow("drawDetectedMarkers", img)
 
-        if cv2.waitKey(1) & 0xFF == ord("q"):
-            break
+class View:
+    log = logger.Logger("View", level=DEBUG)
 
-    # ビデオキャプチャのメモリ解放
-    cap.release()
+    def __init__(self, master, model):
+        self.log.debug("__init__")
 
-    # すべてのウィンドウを閉じる
-    cv2.destroyAllWindows()
+        self.master = master
+        self.model = model
+
+        # フォントの設定
+        self.log.debug("フォントの設定")
+        # ラベルフレーム用
+        self.font_frame = font.Font(family="Meiryo UI", size=15, weight="normal")
+        # ラベル用
+        self.font_label = font.Font(family="Meiryo UI", size=15, weight="bold")
+        # ボタン用
+        self.font_buttom = font.Font(family="Meiryo UI", size=20, weight="bold")
+
+        self.log.debug("フレームの設定")
+        self.frame1 = tk.LabelFrame(self.master, text="元画像", font=self.font_frame)
+        self.frame2 = tk.LabelFrame(self.master, text="計測距離", font=self.font_frame)
+        self.frame3 = tk.Frame(self.master)
+        self.frame4 = tk.LabelFrame(self.master, text="変換画像", font=self.font_frame)
+
+        self.log.debug("グリッドの設定")
+        # c=左, r=上, s=全方向に伸ばす, p=10の隙間
+        self.frame1.grid(column=0, row=0, sticky=tk.E + tk.W + tk.S + tk.N, padx=10, pady=10)
+        # c=左, r=中, s=全方向に伸ばす, p=10の隙間
+        self.frame2.grid(column=0, row=1, sticky=tk.E + tk.W + tk.S + tk.N, padx=10, pady=10)
+        # c=左, r=下, p=10の隙間
+        self.frame3.grid(column=0, row=2, padx=10, pady=10)
+        # c=右, r=上, rp=3個連結, p=10の隙間
+        self.frame4.grid(column=1, row=0, rowspan=3, padx=10, pady=10)
+
+        self.log.debug("各フレームの内部詳細")
+        # フレーム1：オリジナル画像
+        self.canvas1 = tk.Canvas(self.frame1, width=400, height=225)
+        # フレーム２：距離関連
+        self.label211 = tk.Label(self.frame2, text=" X方向 ", font=self.font_label)
+        self.label212 = tk.Label(self.frame2, text=f" {self.model.distance_x:5.1f} ", font=self.font_label)
+        self.label213 = tk.Label(self.frame2, text="mm", font=self.font_label)
+        self.label221 = tk.Label(self.frame2, text=" Y方向 ", font=self.font_label)
+        self.label222 = tk.Label(self.frame2, text=f" {self.model.distance_y:5.1f} ", font=self.font_label)
+        # self.label222 = tk.Label(self.frame2, text="          ", font=self.font_label)
+        self.label223 = tk.Label(self.frame2, text="mm", font=self.font_label)
+        self.label231 = tk.Label(self.frame2, text="  距離", font=self.font_label)
+        self.label232 = tk.Label(self.frame2, text=f" {self.model.distance_xy:5.1f} ", font=self.font_label)
+        self.label233 = tk.Label(self.frame2, text="mm", font=self.font_label)
+        # フレーム３：終了ボタン
+        self.button31 = tk.Button(self.frame3, text="開始", font=self.font_buttom)
+        self.button32 = tk.Button(self.frame3, text="終了", font=self.font_buttom)
+        # フレーム４：変換画像
+        self.canvas4 = tk.Canvas(self.frame4, width=600, height=600)
+
+        self.log.debug("各フレームの内部グリッド")
+        self.canvas1.grid()
+        # c=左1, r=上
+        self.label211.grid(column=0, row=0)
+        # c=左2, r=上
+        self.label212.grid(column=1, row=0)
+        # c=左3, r=上
+        self.label213.grid(column=2, row=0)
+        # c=左1, r=下
+        self.label221.grid(column=0, row=1)
+        # c=左2, r=下
+        self.label222.grid(column=1, row=1)
+        # c=左3, r=下
+        self.label223.grid(column=2, row=1)
+        # c=右1, r=上下
+        self.label231.grid(column=3, row=0, rowspan=2)
+        # c=右2, r=上下
+        self.label232.grid(column=4, row=0, rowspan=2)
+        # c=右3, r=上下
+        self.label233.grid(column=5, row=0, rowspan=2)
+        self.button31.grid(column=0, row=0, padx=20)
+        self.button32.grid(column=1, row=0, padx=20)
+        self.canvas4.grid()
+
+    def display_distance_value(self):
+        self.log.debug("display_distance_value")
+
+        # X方向
+        self.label212 = tk.Label(self.frame2, text=f" {self.model.distance_x:5.1f} ", font=self.font_label)
+        # Y方向
+        self.label222 = tk.Label(self.frame2, text=f" {self.model.distance_y:5.1f} ", font=self.font_label)
+        # 合成距離
+        self.label232 = tk.Label(self.frame2, text=f" {self.model.distance_xy:5.1f} ", font=self.font_label)
+
+        # グリッド
+        self.label212.grid(column=1, row=0)
+        self.label222.grid(column=1, row=1)
+        self.label232.grid(column=4, row=0, rowspan=2)
+
+
+
+    def display_image_original(self):
+        self.img1 = Image.open("test001.png")
+        # 複数のインスタンスがある場合、インスタンをmasterで指示しないとエラーが発生する場合がある
+        # エラー内容：image "pyimage##" doesn't exist
+        self.im1 = ImageTk.PhotoImage(image=self.img1, master=self.frame1)
+        self.canvas1.create_image(0, 0, anchor='nw', image=self.im1)
+
+        self.img4 = Image.open("test002.png")
+        # 複数のインスタンスがある場合、インスタンをmasterで指示しないとエラーが発生する場合がある
+        # エラー内容：image "pyimage##" doesn't exist
+        self.im4 = ImageTk.PhotoImage(image=self.img4, master=self.frame4)
+        self.canvas4.create_image(0, 0, anchor='nw', image=self.im4)
+
+
+class Controller():
+    log = logger.Logger("Controller", level=DEBUG)
+
+    def __init__(self, master, model, view):
+        self.log.debug("__init__")
+
+        # インスタンス化
+        self.master = master
+        self.model = model
+        self.view = view
+
+        # カメラの起動有無
+        self.camera_open = False
+        # 起動するカメラの設定
+        self.camera_setting = json.load(open("setting.json", "r", encoding="utf-8"))
+        self.log.debug(str(self.camera_setting))
+
+
+    def request_camera_open(self):
+        self.log.debug("request_camera_open")
+        # 1回目のみカメラ起動
+        # Model
+        self.log.debug(f"camera_open:{str(self.camera_open)}")
+        if self.camera_open is False:
+            self.log.debug("初回のカメラ起動")
+            self.model.camera_open(self.camera_setting)
+            self.camera_open = True
+
+    def request_camera_results(self):
+        self.log.debug("request_camera_view")
+
+        # カメラ処理（画像取得、距離取得）の実施
+        # Moldeクラス
+        self.model.create_camera_infomations()
+
+        # 画像取得
+        # Viewクラス
+        self.view.display_distance_value()
+
+        # 数値出力
+        # Viewクラス
+
+        # 繰り返し動作
+        self.master.after(100, self.request_camera_results)
+
+
+    def press_start_button(self):
+        self.log.debug("press_start_button")
+
+        # カメラの起動
+        self.request_camera_open()
+
+        # カメラの結果を取得
+        self.request_camera_results()
+
+
+
+    def press_close_button(self):
+        self.log.debug("press_close_button")
+        self.master.destroy()
+        # カメラリソース解放
+        self.model.caremra_release()
+        
+        self.log.debug("終了")
+
+
+
+
+class Application(tk.Frame):
+    log = logger.Logger("Application", level=DEBUG)
+
+    def __init__(self, master):
+        # tkinterの定型文
+
+        self.log.debug("__init__")
+        super().__init__(master)
+        self.grid()
+
+        self.log.debug("Modelのインスタンス化")
+        self.model = Model()
+
+        self.log.debug("ウインドウ作成")
+        master.geometry("1060x660")
+        master.title("カメラによる計測アプリ")
+
+        self.log.debug("Viewのインスタンス化")
+        self.view = View(master, self.model)
+
+        self.log.debug("Controllerのインスタンス化")
+        self.controller = Controller(master, self.model, self.view)
+
+        self.log.debug("ボタンの登録")
+        self.view.button31["command"] = self.controller.press_start_button
+        self.view.button32["command"] = self.controller.press_close_button
+        
 
 
 def main():
-    # パラメータの取り出し
-    setting = open("setting.json", "r", encoding="utf-8")
-    setting_dict = json.load(setting)
+    win = tk.Tk()
+    app = Application(master=win)
+    app.mainloop()
 
-    log = logger.Logger("main", level=INFO)
-    log.debug("test")
-
-    estimation(setting_dict)
 
 if __name__ == "__main__":
     main()
+
+# 参考資料
+
+# 【Python】Tkinterを使った雛形（MVCモデル）
+# https://qiita.com/michimichix521/items/8687962247cae41625f7
+
+# python: サンプルプログラムでMVCモデルの構成を掴む
+# https://moimoiblog.com/programing/python-mvcmodel-study/
+
+# Tkinter GUIアプリケーションの部品 (widgets) をウィンドウ上にどうやって配置するのだろう - 3つのジオメトリマネージャー
+# https://cassiopeia.hatenadiary.org/entry/20070905/1189023758
+
+# 【Python】Tkinterのcanvasを使ってみる
+# https://qiita.com/nnahito/items/2ab3ad0f3adacc3314e6
+
+# PythonでGUIに画像を表示する
+# https://water2litter.net/rum/post/python_tkinter_canvas_create_image/
+
+# 【Python tkinter】LabelFrame（ラベルフレーム）ウィジェットの使い方
+# https://office54.net/python/tkinter/python-tkinter-labelframe
--- 022_sample03.py
+++ 022_sample03.py
@@ -2,6 +2,11 @@ import tkinter as tk
 from PIL import Image, ImageTk
 from tkinter import font
 
+import cv2
+import numpy as np
+
+import json
+
 
 # PEP8に準拠するとimportが先頭に行くので苦肉の策
 while True:
@@ -17,6 +22,113 @@ class Model:
 
     def __init__(self):
         self.log.debug("__init__")
+        self.distance_x = 999.0
+        self.distance_y = 999.0
+        self.distance_xy = 999.0
+
+    def camera_open(self, camera_setting):
+        # カメラ起動
+        self.log.debug("camera_open")
+
+        self.camera_setting = camera_setting
+
+        self.aruco = cv2.aruco
+        self.dictionary = self.aruco.getPredefinedDictionary(self.aruco.DICT_4X4_50)
+
+        self.cap = cv2.VideoCapture(self.camera_setting["CAM"]["ID"])
+        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, self.camera_setting["CAM"]["WIDTH"])
+    
+    def caremra_release(self):
+        # カメラリソース解放
+        self.log.debug("camera_release")
+        self.cap.release()
+
+    def create_camera_infomations(self):
+        # cv2の処理をすべて実施
+        self.log.debug("create_camera_infomations")
+
+        # ビデオキャプチャから画像を取得
+        ret, frame = self.cap.read()
+
+        self.log.debug(ret)
+
+        # sizeを取得
+        # (縦、横、色)
+
+        Height, Width = frame.shape[:2]
+
+        img0 = cv2.resize(frame, (int(200), int(200 * Height / Width)))
+        img1 = cv2.resize(frame, (int(Width), int(Height)))
+
+        # 検出
+        corners, ids, rejectedImgPoints = self.aruco.detectMarkers(img1, self.dictionary)
+
+        # 検出したマーカに描画する
+        # イメージをコピーする
+        self.img2 = np.copy(img1)
+        self.aruco.drawDetectedMarkers(self.img2, corners, ids, (0, 255, 0))
+        self.img2 = cv2.resize(self.img2, (int(400), int(400 * Height / Width)))
+
+        self.log.debug(f"x:400 y:{int(400* Height / Width)}")
+
+        m = np.empty((4, 2))
+
+        self.log.debug(ids)
+        self.log.debug(type(ids))
+
+        # ラインを引く準備
+        x_start, y_start = 0, 0
+        x_end, y_end = 0, 0
+
+        if ids is None:
+            img_trans = cv2.resize(frame, (600, 600))
+            self.log.debug("None")
+        elif np.count_nonzero((2 <= ids) & (ids <= 6)) == 4:
+            self.log.debug("Number is 4")
+            for i, c in zip(ids, corners):
+                # マーカーの赤丸位置を基準としている
+                if 2 <= i <= 6:
+                    self.log.debug(c)
+                    m[i - 2] = c[0][0]
+
+            # 変形後の画像サイズ
+            trans_width, trans_height = (600, 600)
+
+            marker_coordinates = np.float32(m)
+            true_coordinates = np.float32([[0, 0], [trans_width, 0], [trans_width, trans_height], [0, trans_height]])
+            trans_mat = cv2.getPerspectiveTransform(marker_coordinates, true_coordinates)
+            self.img_trans = cv2.warpPerspective(img1, trans_mat, (trans_width, trans_height))
+
+            if np.count_nonzero((0 <= ids) & (ids <= 1)) == 2:
+                # 変換後の画像でid検索
+                corners_trans, ids_trans, rejectedImgPoints_trans = self.aruco.detectMarkers(self.img_trans, self.dictionary)
+                # id(0と1)の抽出
+                if ids_trans is not None:
+                    for number, corner in zip(ids_trans, corners_trans):
+                        if number == 0:
+                            x_start, y_start = int(corner[0][0][0]), int(corner[0][0][1])
+                        elif number == 1:
+                            x_end, y_end = int(corner[0][0][0]), int(corner[0][0][1])
+                    self.log.debug(x_start)
+                    self.log.debug(y_start)
+                    self.log.debug(x_end)
+                    self.log.debug(y_end)
+                    # id(0と1)の距離を表示
+                    self.distance_x, self.distance_y = (x_end - x_start), (y_end - y_start)
+                    self.log.info(self.distance_x)
+                    self.log.info(self.distance_y)
+                    self.distance_xy = ((self.distance_x * self.distance_x + self.distance_y * self.distance_y) ** 0.5) * 135 / 600
+                    self.log.info(self.distance_xy)
+
+                    cv2.line(self.img_trans, (x_start, y_start), (x_end, y_end), (0, 0, 255), 1)
+
+        else:
+            self.log.debug("others")
+            self.img_trans = cv2.resize(frame, (600, 600))
+            self.distance_x = 0.0
+            self.distance_y = 0.0
+            self.distance_xy = 0.0
+            
 
 
 class View:
@@ -57,14 +169,15 @@ class View:
         # フレーム1：オリジナル画像
         self.canvas1 = tk.Canvas(self.frame1, width=400, height=225)
         # フレーム２：距離関連
-        self.label211 = tk.Label(self.frame2, text="　X方向　", font=self.font_label)
-        self.label212 = tk.Label(self.frame2, text="0.0", font=self.font_label)
+        self.label211 = tk.Label(self.frame2, text=" X方向 ", font=self.font_label)
+        self.label212 = tk.Label(self.frame2, text=f" {self.model.distance_x:5.1f} ", font=self.font_label)
         self.label213 = tk.Label(self.frame2, text="mm", font=self.font_label)
-        self.label221 = tk.Label(self.frame2, text="　Y方向　", font=self.font_label)
-        self.label222 = tk.Label(self.frame2, text="80.0", font=self.font_label)
+        self.label221 = tk.Label(self.frame2, text=" Y方向 ", font=self.font_label)
+        self.label222 = tk.Label(self.frame2, text=f" {self.model.distance_y:5.1f} ", font=self.font_label)
+        # self.label222 = tk.Label(self.frame2, text="          ", font=self.font_label)
         self.label223 = tk.Label(self.frame2, text="mm", font=self.font_label)
-        self.label231 = tk.Label(self.frame2, text="　　　距離", font=self.font_label)
-        self.label232 = tk.Label(self.frame2, text="128.1", font=self.font_label)
+        self.label231 = tk.Label(self.frame2, text="  距離", font=self.font_label)
+        self.label232 = tk.Label(self.frame2, text=f" {self.model.distance_xy:5.1f} ", font=self.font_label)
         self.label233 = tk.Label(self.frame2, text="mm", font=self.font_label)
         # フレーム３：終了ボタン
         self.button31 = tk.Button(self.frame3, text="開始", font=self.font_buttom)
@@ -96,14 +209,24 @@ class View:
         self.button32.grid(column=1, row=0, padx=20)
         self.canvas4.grid()
 
-    def view_camera_image(self):
-        self.log.debug("画像表示")
+    def display_distance_value(self):
+        self.log.debug("display_distance_value")
 
-        self.label212 = tk.Label(self.frame2, text="100.0", font=self.font_label)
+        # X方向
+        self.label212 = tk.Label(self.frame2, text=f" {self.model.distance_x:5.1f} ", font=self.font_label)
+        # Y方向
+        self.label222 = tk.Label(self.frame2, text=f" {self.model.distance_y:5.1f} ", font=self.font_label)
+        # 合成距離
+        self.label232 = tk.Label(self.frame2, text=f" {self.model.distance_xy:5.1f} ", font=self.font_label)
+
+        # グリッド
         self.label212.grid(column=1, row=0)
+        self.label222.grid(column=1, row=1)
+        self.label232.grid(column=4, row=0, rowspan=2)
 
 
 
+    def display_image_original(self):
         self.img1 = Image.open("test001.png")
         # 複数のインスタンスがある場合、インスタンをmasterで指示しないとエラーが発生する場合がある
         # エラー内容：image "pyimage##" doesn't exist
@@ -123,17 +246,64 @@ class Controller():
     def __init__(self, master, model, view):
         self.log.debug("__init__")
 
+        # インスタンス化
         self.master = master
         self.model = model
         self.view = view
 
+        # カメラの起動有無
+        self.camera_open = False
+        # 起動するカメラの設定
+        self.camera_setting = json.load(open("setting.json", "r", encoding="utf-8"))
+        self.log.debug(str(self.camera_setting))
+
+
+    def request_camera_open(self):
+        self.log.debug("request_camera_open")
+        # 1回目のみカメラ起動
+        # Model
+        self.log.debug(f"camera_open:{str(self.camera_open)}")
+        if self.camera_open is False:
+            self.log.debug("初回のカメラ起動")
+            self.model.camera_open(self.camera_setting)
+            self.camera_open = True
+
+    def request_camera_results(self):
+        self.log.debug("request_camera_view")
+
+        # カメラ処理（画像取得、距離取得）の実施
+        # Moldeクラス
+        self.model.create_camera_infomations()
+
+        # 画像取得
+        # Viewクラス
+        self.view.display_distance_value()
+
+        # 数値出力
+        # Viewクラス
+
+        # 繰り返し動作
+        self.master.after(100, self.request_camera_results)
+
+
     def press_start_button(self):
         self.log.debug("press_start_button")
-        self.view.view_camera_image()
+
+        # カメラの起動
+        self.request_camera_open()
+
+        # カメラの結果を取得
+        self.request_camera_results()
+
+
 
     def press_close_button(self):
         self.log.debug("press_close_button")
         self.master.destroy()
+        # カメラリソース解放
+        self.model.caremra_release()
+        
+        self.log.debug("終了")
 
 
 
@@ -161,7 +331,7 @@ class Application(tk.Frame):
         self.log.debug("Controllerのインスタンス化")
         self.controller = Controller(master, self.model, self.view)
 
-        self.log.debug("ストップコマンドの登録")
+        self.log.debug("ボタンの登録")
         self.view.button31["command"] = self.controller.press_start_button
         self.view.button32["command"] = self.controller.press_close_button
         
